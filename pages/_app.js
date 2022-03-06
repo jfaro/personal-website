fd96a7eda3cb8645ebbe45d0b4c4ddbd730d91a3
jfaro <jfaro@umich.edu>
2022-03-05 20:19:36 -0500

    Add page loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
-import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+import { Router } from 'next/router'
 import '../styles/globals.css'
+import '../styles/nprogress.css';
 
+// Progress bar
+NProgress.configure({ showSpinner: false });
+Router.events.on()
+Router.events.on("routeChangeStart", NProgress.start);
+Router.events.on("routeChangeError", NProgress.done);
+Router.events.on("routeChangeComplete", NProgress.done);
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
   return <Component {...pageProps} />
 }
 
