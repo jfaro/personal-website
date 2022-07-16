983e6550c23487fca1b5633ee6eb5c53a5048e4e
jfaro <jfaro@umich.edu>
2022-07-16 10:10:32 -0700

    Update Button styling to use sass module

--- components/Button.js
+++ components/Button.js
@@ -1,51 +1,16 @@
-import React from 'react'
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: .75rem 2rem;
-    border: 1px solid var(--textPrimary);
-    border-radius: 2px;
-
-    transition: .4s;
-    :hover {
-        transform: translateY(-4px);
-        cursor: pointer;
-    }
-
-    @media (max-width: 768px) {
-        width: 100%;
-    }
-`
-
-const FilledButton = styled(StyledButton)`
-    background: var(--textPrimary);
-    color: var(--bgRaised);
-    > svg {
-        fill: var(--bgRaised);
-        stroke: var(--bgRaised);
-    }
-`
-
-const OutlineButton = styled(StyledButton)`
-    background: none;
-    color: var(--textPrimary);
-    
-`
+import cx from "classnames";
+import styles from "./Button.module.scss";
 
 export default function Button({ url, outlined, icon, children }) {
 
-    const paddedIcon = icon ? <React.Fragment>{icon} &nbsp;</React.Fragment> : null
-
-    const ButtonType = outlined ?
-        <OutlineButton>{paddedIcon} {children}</OutlineButton> :
-        <FilledButton>{paddedIcon} {children}</FilledButton>
+    const classes = cx(styles.button, {
+        [styles.filled]: !outlined,
+        [styles.outlined]: outlined
+    })
 
     return (
         <a href={url} target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>
-            {ButtonType}
+            <div className={classes}>{icon && <>{icon} &nbsp;</>}{children}</div>
         </a>
     )
 }
