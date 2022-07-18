a6129cb461fd8f0779f38af02353cf1274a87621
jfaro <jfaro@umich.edu>
2022-07-17 17:49:48 -0700

    Remove links from document head

--- pages/_document.js
+++ pages/_document.js
@@ -34,17 +34,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-
-                    {/* Google Fonts */}
-                    <link
-                        rel="preconnect"
-                        href="https://fonts.googleapis.com"
-                    />
-                    <link
-                        rel="preconnect"
-                        href="https://fonts.gstatic.com"
-                        crossOrigin='true'
-                    />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap"
                         rel="stylesheet"
