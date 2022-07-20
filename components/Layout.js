171475aa8cc99bdb881395e4d842e9267e372be8
jfaro <jfaro@umich.edu>
2022-07-19 22:51:21 -0700

    Refactor Layout to use scss module

--- components/Layout.js
+++ components/Layout.js
@@ -1,52 +1,19 @@
 import Head from 'next/head'
-import * as React from 'react'
-import styled from 'styled-components'
 import Header from './Header'
-
-const Container = styled.div`
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background: var(--bg);
-    padding: 0 4rem 4rem 4rem;
-    overflow: hidden;
-
-    @media(max-width: 800px) {
-        padding: 0 2rem 2rem 2rem;
-    }
-`
-
-const Content = styled.div`
-    width: 100%;
-    max-width: 1200px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    > * + * {
-        margin-top: 4rem;
-    }
-
-    @media(max-width: 800px) {
-        > * + * {
-        margin-top: 4rem;
-    }
-    }
-`
+import styles from "./Layout.module.scss";
 
 const Layout = ({ pageTitle, children }) => {
     return (
-        <Container>
+        <div className={styles.container}>
             <Head>
                 <title>Julien Faro</title>
             </Head>
-            <Content>
+            <div className={styles.content}>
                 <Header />
                 {pageTitle ? <h1>{pageTitle}</h1> : null}
                 {children}
-            </Content>
-        </Container>
+            </div>
+        </div>
     )
 }
 export default Layout
