78786a49ce4932973dc6892abd564023c9e55f39
jfaro <jfaro@umich.edu>
2022-07-19 21:03:25 -0700

    Remove nprogress from _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,4 @@
-import NProgress from 'nprogress'
-import { Router } from 'next/router'
 import '../styles/globals.css'
-import '../styles/nprogress.css';
-
-// Progress bar
-NProgress.configure({ showSpinner: false });
-Router.events.on()
-Router.events.on("routeChangeStart", NProgress.start);
-Router.events.on("routeChangeError", NProgress.done);
-Router.events.on("routeChangeComplete", NProgress.done);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
